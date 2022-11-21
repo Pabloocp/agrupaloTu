009c826239fa4074f3fdde6ecb8b4581bc3bfc64
Pabloocp <pablocastropaz00@gmail.com>
2022-11-21 10:05:59 +0100

    tests

--- test/vitertest.test.js
+++ test/vitertest.test.js
@@ -4,7 +4,8 @@ import {encuntraarray} from "../Utils"
 test('crear grupos',()=>{
     const arrayTest = ["paco","maria","antonio"]
     const num = 2
-     const value =  encuntraarray(arrayTest,num)
-     expect(value.length).toBe(2)
-    expect(value[1]).toBeDefined
+    const value =  encuntraarray(arrayTest,num)
+    expect(value.length).toBe(2)
+    expect(value[0]).toHaveLength(2)
+    expect(value[1]).toHaveLength(1)
 })
